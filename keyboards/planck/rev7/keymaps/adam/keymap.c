64b5c84c85db5e5005aa02467953074ec2929f22
Adam Pitzele <pitzelea@reninc.com>
2023-08-16 09:08:53 -0400

    switch lower and ctrl

--- keyboards/planck/rev7/keymaps/adam/keymap.c
+++ keyboards/planck/rev7/keymaps/adam/keymap.c
@@ -1,19 +1,3 @@
-/* Copyright 2015-2023 Jack Humbert
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 
 enum planck_layers { _COLEMAK, _LOWER, _RAISE, _ADJUST };
@@ -116,21 +100,21 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Colemak DH
- * ,-------------------------------------------------------------------------------------------.
- * | Tab    |   Q  |   W  |   F  |   P  |   B  |   J  |    L   |   U   |   Y  |   ;    | Esc   |
- * |--------+------+------+------+------+------+------+--------+-------+------+--------+-------|
- * | Bksp   |   A  |   R  |   S  |   T  |   G  |   M  |    N   |   E   |   I  |   O    |  '    |
- * |--------+------+------+------+------+------+------+--------+-------+------+--------+-------|
- * | Shift  |   Z  |   X  |   C  |   D  |   V  |   K  |    H   |   ,   |   .  |   /    | Del   |
- * |--------+------+------+------+------+------+------+--------+-------+------+--------+-------|
- * |        |      |      | Alt  | Ctrl |    Space*   | Raise* |  Win  |      |        |       |
- * `-------------------------------------------------------------------------------------------'
+ * ,-------------------------------------------------------------------------------------------------.
+ * | Tab    |   Q  |   W  |   F  |   P   |   B  |   J  |      L      |   U   |   Y  |   ;    | Esc   |
+ * |--------+------+------+------+-------+------+------+-------------+-------+------+--------+-------|
+ * | Bksp   |   A  |   R  |   S  |   T   |   G  |   M  |      N      |   E   |   I  |   O    |  '    |
+ * |--------+------+------+------+-------+------+------+-------------+-------+------+--------+-------|
+ * | Shift  |   Z  |   X  |   C  |   D   |   V  |   K  |      H      |   ,   |   .  |   /    | Del   |
+ * |--------+------+------+------+-------+------+------+-------------+-------+------+--------+-------|
+ * |        |      |      | Alt  | Lower | Space/Ctrl  | Enter/Raise |  Win  |      |        |       |
+ * `-------------------------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_planck_mit(
-    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,     KC_J,    KC_L,              KC_U,    KC_Y,    KC_SCLN, KC_ESC,
-    KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,     KC_M,    KC_N,              KC_E,    KC_I,    KC_O,    KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,     KC_K,    KC_H,              KC_COMM, KC_DOT,  KC_SLSH, KC_DEL,
-    _______, _______, KC_LGUI, KC_LALT, KC_LCTL, LT(LOWER, KC_SPC), LT(RAISE, KC_ENT), KC_LGUI, _______, _______, _______
+    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,  KC_J,          KC_L,              KC_U,     KC_Y,    KC_SCLN, KC_ESC,
+    KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,  KC_M,          KC_N,              KC_E,     KC_I,    KC_O,    KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,  KC_K,          KC_H,              KC_COMM,  KC_DOT,  KC_SLSH, KC_DEL,
+    _______, _______, KC_LGUI, KC_LALT, LOWER,   MT(MOD_LCTL, KC_SPC), LT(RAISE, KC_ENT), KC_LGUI,  _______, _______, _______
 ),
 
 /* Lower
@@ -189,12 +173,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 
 };
-/* clang-format on */
-
-#ifdef AUDIO_ENABLE
-float plover_song[][2]    = SONG(PLOVER_SOUND);
-float plover_gb_song[][2] = SONG(PLOVER_GOODBYE_SOUND);
-#endif
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
@@ -259,85 +237,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return true;
             break;
-    }
-    return true;
-}
-
-/* clang-format off */
-float melody[8][2][2] = {
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-};
-/* clang-format on */
-
-#define JUST_MINOR_THIRD 1.2
-#define JUST_MAJOR_THIRD 1.25
-#define JUST_PERFECT_FOURTH 1.33333333
-#define JUST_TRITONE 1.42222222
-#define JUST_PERFECT_FIFTH 1.33333333
-
-#define ET12_MINOR_SECOND 1.059463
-#define ET12_MAJOR_SECOND 1.122462
-#define ET12_MINOR_THIRD 1.189207
-#define ET12_MAJOR_THIRD 1.259921
-#define ET12_PERFECT_FOURTH 1.33484
-#define ET12_TRITONE 1.414214
-#define ET12_PERFECT_FIFTH 1.498307
-
-deferred_token tokens[8];
-
-uint32_t reset_note(uint32_t trigger_time, void *note) {
-    *(float*)note = 440.0f;
-    return 0;
-}
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    cancel_deferred_exec(tokens[index]);
-    if (clockwise) {
-        melody[index][1][0] = melody[index][1][0] * ET12_MINOR_SECOND;
-        melody[index][0][0] = melody[index][1][0] / ET12_PERFECT_FIFTH;
-        audio_play_melody(&melody[index], 2, false);
-    } else {
-        melody[index][1][0] = melody[index][1][0] / ET12_MINOR_SECOND;
-        melody[index][0][0] = melody[index][1][0] * ET12_TRITONE;
-        audio_play_melody(&melody[index], 2, false);
-    }
-    tokens[index] = defer_exec(1000, reset_note, &melody[index][1][0]);
-    return false;
-}
-
-bool dip_switch_update_user(uint8_t index, bool active) {
-    switch (index) {
-        case 0: {
-#ifdef AUDIO_ENABLE
-            static bool play_sound = false;
-#endif
-            if (active) {
-#ifdef AUDIO_ENABLE
-                if (play_sound) {
-                    PLAY_SONG(plover_song);
-                }
-#endif
-                layer_on(_ADJUST);
-            } else {
-#ifdef AUDIO_ENABLE
-                if (play_sound) {
-                    PLAY_SONG(plover_gb_song);
-                }
-#endif
-                layer_off(_ADJUST);
-            }
-#ifdef AUDIO_ENABLE
-            play_sound = true;
-#endif
-            break;
-        }
     }
     return true;
 }
