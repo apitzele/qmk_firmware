cec1418fde04457ca45731e393927a6ba8c4b480
Adam Pitzele <pitzelea@reninc.com>
2023-11-27 09:20:27 -0500

    tab combo

--- keyboards/crkbd/keymaps/apitzele/keymap.c
+++ keyboards/crkbd/keymaps/apitzele/keymap.c
@@ -7,6 +7,16 @@ enum layers { _MAIN, _SYMR, _SYML, _NUM, _NAV };
 #define NAV MO(_NAV)
 #define NUM MO(_NUM)
 
+enum combos { 
+  TAB
+};
+
+const uint16_t PROGMEM tab_combo[] = {LT(NUM, KC_SPC), LT(NAV, KC_ENT), COMBO_END};
+
+combo_t key_combos[] = {
+    [TAB] = COMBO(tab_combo, KC_TAB)
+};
+
 //Tap dances
 enum {
   T_LT_HM,
@@ -17,12 +27,9 @@ enum {
   T_D,
   T_V,
   T_F,
-  T_Y,
   T_H,
   T_Q,
   T_W,
-  T_TAB,
-  T_DEL,
   T_SLASH
 };
 
@@ -32,11 +39,7 @@ void slash_reset (tap_dance_state_t *state, void *user_data);
 tap_dance_action_t tap_dance_actions[] = {
   [T_LT_HM] = ACTION_TAP_DANCE_TAP_AND_HOLD(KC_LEFT, KC_HOME),
   [T_RT_ED] = ACTION_TAP_DANCE_TAP_AND_HOLD(KC_RIGHT, KC_END),
-  [T_TAB] = ACTION_TAP_DANCE_TAP_AND_HOLD(KC_TAB, LSFT(KC_TAB)),
-  [T_DEL] = ACTION_TAP_DANCE_TAP_AND_HOLD (KC_DEL, RSFT(KC_DEL)),
   [T_SLASH] = ACTION_TAP_DANCE_FN_ADVANCED (NULL, slash_finished, slash_reset),
-  [T_Z] = ACTION_TAP_DANCE_TAP_AND_HOLD(KC_Z, LCTL(KC_Z)),
-  [T_Y] = ACTION_TAP_DANCE_TAP_AND_HOLD(KC_Y, RCTL(KC_Y)),
   [T_X] = ACTION_TAP_DANCE_TAP_HOLD(KC_X, LCTL(KC_X)),
   [T_C] = ACTION_TAP_DANCE_TAP_HOLD(KC_C, LCTL(KC_C)),
   [T_D] = ACTION_TAP_DANCE_TAP_HOLD(KC_D, LCTL(KC_S)), // Hold D = Ctrl + S
@@ -103,36 +106,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAIN] = LAYOUT_split_3x6_3(
  //,-------------------------------------------------------------------------.         ,----------------------------------------------------------------------.
-    XXXXXXX, TD(T_Q),      TD(T_W),      TD(T_F),      KC_P,         KC_B,               KC_J, KC_L,         KC_U,         TD(T_Y),      KC_SCLN,      XXXXXXX,
+    XXXXXXX, TD(T_Q),      TD(T_W),      TD(T_F),      KC_P,         KC_B,               KC_J, KC_L,         KC_U,         KC_Y,      KC_SCLN,      XXXXXXX,
  //|-------+-------------+-------------+-------------+-------------+---------|         |-----+-------------+-------------+-------------+-------------+--------|
     XXXXXXX, LCTL_T(KC_A), LALT_T(KC_R), LGUI_T(KC_S), LSFT_T(KC_T), KC_G,               KC_M, RSFT_T(KC_N), RGUI_T(KC_E), RALT_T(KC_I), RCTL_T(KC_O), XXXXXXX,
  //|-------+-------------+-------------+-------------+-------------+---------|         |-----+-------------+-------------+-------------+-------------+--------|
-    XXXXXXX, TD(T_Z),      TD(T_X),      TD(T_C),      TD(T_D),      TD(T_V),            KC_K, TD(T_H),      KC_COMM,      KC_DOT,       TD(T_SLASH),  XXXXXXX,
+    XXXXXXX, KC_Z,      TD(T_X),      TD(T_C),      TD(T_D),      TD(T_V),            KC_K, TD(T_H),      KC_COMM,      KC_DOT,       TD(T_SLASH),  XXXXXXX,
  //,-----------------------------------------------------------------------------|  |-------------------------------------------------------------------------.
                                      _______, LT(SYMR, KC_BSPC), LT(NUM, KC_SPC),     LT(NAV, KC_ENT), LT(SYML, KC_DEL),   _______
 
   ),
 
   [_SYMR] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.          ,-----------------------------------------------------.
-      XXXXXXX, _______, _______, _______, _______, _______,            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______,            _______, KC_LCBR, KC_RCBR, KC_QUOT, KC_DQT,  XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, _______, _______, _______, _______, _______,            _______, KC_LBRC, KC_RBRC, KC_GRV,  KC_BSLS, XXXXXXX,
-  //,---------------------------------------------------------|  |---------------------------------------------------------.
-                                    _______, _______, _______,     KC_ENT, KC_DEL, _______
+  //,--------------------------------------------------------.          ,-----------------------------------------------------.
+      XXXXXXX, _______, _______,    _______, _______, _______,            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
+  //|--------+--------+-----------+--------+--------+--------|          |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, KC_LCTL, KC_LALT,    KC_LGUI, KC_LSFT, _______,            _______, KC_LCBR, KC_RCBR, KC_QUOT, KC_DQT,  XXXXXXX,
+  //|--------+--------+-----------+--------+--------+--------|          |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, LCTL(KC_Z), _______, _______, _______, _______,            _______, KC_LBRC, KC_RBRC, KC_GRV,  KC_BSLS, XXXXXXX,
+  //,-------------------------------------------------  ---------|  |---------------------------------------------------------.
+                                       _______, _______, _______,     KC_ENT, KC_DEL, _______
   ),
 
   [_SYML] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.          ,-----------------------------------------------------.
-      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,            _______, _______, _______, _______, _______, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------+--------|
-      _______, KC_EQL,  KC_PLUS, KC_MINS, KC_UNDS, _______,            KC_RSFT, KC_RGUI, KC_RALT, KC_RCTL, _______, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+----|  |--------+--------+--------+--------+--------+--------+--------|
-                                      _______, KC_BSPC, KC_SPC,     _______, _______, _______
+  //,-----------------------------------------------------.          ,--------------------------------------------------------.
+      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,            _______, _______, _______, _______, _______,    XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|          |--------+--------+--------+--------+-----------+--------|
+      _______, KC_EQL,  KC_PLUS, KC_MINS, KC_UNDS, _______,            KC_RSFT, KC_RGUI, KC_RALT, KC_RCTL, _______,    XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|          |--------+--------+--------+--------+-----------+--------|
+      _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, LCTL(KC_Y), XXXXXXX,
+  //,-------------------------------------------------  -------|  |---------------------------------------------------------.
+                                     _______, KC_BSPC, KC_SPC,     _______, _______, _______
   ),
 
   [_NUM] = LAYOUT_split_3x6_3(
