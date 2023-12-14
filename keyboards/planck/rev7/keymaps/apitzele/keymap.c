d737854088a17a69dfc70aed08f2c172777caa37
Adam Pitzele <pitzelea@reninc.com>
2023-12-13 22:02:20 -0500

    use shared layout for planck

--- keyboards/planck/rev7/keymaps/apitzele/keymap.c
+++ keyboards/planck/rev7/keymaps/apitzele/keymap.c
@@ -1,59 +1,38 @@
 #include QMK_KEYBOARD_H
-#include "layers.h"
 #include "tapdances.h"
-#include "combos.h"
+#include "layout34.h"
 
 // FLASH: qmk flash -kb planck/rev7 -km apitzele
 
-/* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MAIN] = LAYOUT_planck_grid(
-    TD(T_Q),      TD(T_W),      TD(T_F),      KC_P,           KC_B,            XXXXXXX, XXXXXXX,  KC_J,            KC_L,           KC_U,         KC_Y,         KC_SCLN,
- //|------------+-------------+-------------+---------------+---------------+                   +----------------+---------------+-------------+-------------+-------------|
-    LCTL_T(KC_A), LSFT_T(KC_R), LALT_T(KC_S), LT(SYMR, KC_T), KC_G,            XXXXXXX, XXXXXXX,  KC_M,            LT(SYML, KC_N), LALT_T(KC_E), RSFT_T(KC_I), RCTL_T(KC_O),
- //|------------+-------------+-------------+---------------+---------------+                   +-----+----------+---------------+-------------+-------------+-------------|
-    KC_Z,         TD(T_X),      TD(T_C),      TD(T_D),        TD(T_V),         XXXXXXX, XXXXXXX,  KC_K,            TD(T_H),        KC_COMM,      KC_DOT,       TD(T_SLASH),
- //,------------------------------------------------------------------------+                   +--------------------------------------------------------------------------.
-    XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_BSPC,        LT(NUM, KC_SPC), XXXXXXX, XXXXXXX,  LT(NAV, KC_ENT), TD(T_DEL),      XXXXXXX,      XXXXXXX,      XXXXXXX
+    layout.dft[0][0], layout.dft[0][1], layout.dft[0][2], layout.dft[0][3], layout.dft[0][4], XXXXXXX, XXXXXXX, layout.dft[0][5], layout.dft[0][6], layout.dft[0][7], layout.dft[0][8], layout.dft[0][9],
+    layout.dft[1][0], layout.dft[1][1], layout.dft[1][2], layout.dft[1][3], layout.dft[1][4], XXXXXXX, XXXXXXX, layout.dft[1][5], layout.dft[1][6], layout.dft[1][7], layout.dft[1][8], layout.dft[1][9],
+    layout.dft[2][0], layout.dft[2][1], layout.dft[2][2], layout.dft[2][3], layout.dft[2][4], XXXXXXX, XXXXXXX, layout.dft[2][5], layout.dft[2][6], layout.dft[2][7], layout.dft[2][8], layout.dft[2][9],
+    XXXXXXX,           XXXXXXX,         XXXXXXX,          layout.dft[3][0], layout.dft[3][1], XXXXXXX, XXXXXXX, layout.dft[3][2], layout.dft[3][3], XXXXXXX,          XXXXXXX,          XXXXXXX
   ),
-
-  [_SYMR] = LAYOUT_planck_grid(
-      _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-  //|--------+--------+--------+--------+--------+                   +-------+--------+--------+--------+--------|
-      KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, _______, XXXXXXX, XXXXXXX,  _______, KC_LCBR, KC_RCBR, KC_QUOT, KC_DQT,
-  //|--------+--------+--------+--------+--------+                   +-------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX,  _______, KC_LBRC, KC_RBRC, KC_GRV,  KC_BSLS,
-  //,--------------------------------------------+                   +-------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX,  KC_ENT,  KC_DEL,  XXXXXXX, XXXXXXX, XXXXXXX
-  ),
-
-  [_SYML] = LAYOUT_planck_grid(
-      KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, XXXXXXX, XXXXXXX,  _______, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+                   +-------+--------+--------+--------+--------|
-      KC_EQL,  KC_PLUS, KC_MINS, KC_UNDS, _______, XXXXXXX, XXXXXXX,  _______, KC_RGUI, KC_RALT, KC_RSFT, KC_RCTL,
-  //|--------+--------+--------+--------+--------+                   +-------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX,  _______, _______, _______, _______, _______,
-  //,--------------------------------------------+                   +-------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC, KC_SPC,  XXXXXXX, XXXXXXX,  _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX
-  ),
-
   [_NUM] = LAYOUT_planck_grid(
-      _______,    _______,    _______, QK_BOOT, _______, XXXXXXX, XXXXXXX,  KC_PAST, KC_P7,  KC_P8,   KC_P9,   KC_PSLS,
-  //|-----------+-----------+--------+--------+--------+                   +-------+--------+--------+--------+--------|
-      KC_LCTL,    KC_LSFT,    KC_LALT, KC_LGUI, _______, XXXXXXX, XXXXXXX,  KC_PPLS, KC_P4,  KC_P5,   KC_P6,   KC_PMNS,
-  //|-----------+-----------+--------+--------+--------+                   +-------+--------+--------+--------+--------|
-      LCTL(KC_Z), LCTL(KC_Y), _______, _______, _______, XXXXXXX, XXXXXXX,  KC_PDOT, KC_P1,  KC_P2,   KC_P3,   KC_PEQL,
-  //,--------------------------------------------+                         +-------------------------------------------.
-      XXXXXXX,    XXXXXXX,    XXXXXXX, _______, _______, XXXXXXX, XXXXXXX,  KC_P0, _______,  XXXXXXX, XXXXXXX, XXXXXXX
+    layout.num[0][0], layout.num[0][1], layout.num[0][2], layout.num[0][3], layout.num[0][4], XXXXXXX, XXXXXXX, layout.num[0][5], layout.num[0][6], layout.num[0][7], layout.num[0][8], layout.num[0][9],
+    layout.num[1][0], layout.num[1][1], layout.num[1][2], layout.num[1][3], layout.num[1][4], XXXXXXX, XXXXXXX, layout.num[1][5], layout.num[1][6], layout.num[1][7], layout.num[1][8], layout.num[1][9],
+    layout.num[2][0], layout.num[2][1], layout.num[2][2], layout.num[2][3], layout.num[2][4], XXXXXXX, XXXXXXX, layout.num[2][5], layout.num[2][6], layout.num[2][7], layout.num[2][8], layout.num[2][9],
+    XXXXXXX,           XXXXXXX,         XXXXXXX,          layout.num[3][0], layout.num[3][1], XXXXXXX, XXXXXXX, layout.num[3][2], layout.num[3][3], XXXXXXX,          XXXXXXX,          XXXXXXX
   ),
-
-    [_NAV] = LAYOUT_planck_grid(
-      _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX,  _______, _______,     _______, _______, _______,
-  //|--------+--------+--------+--------+--------+                  +--------+------------+--------+--------+-------------|
-      KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, _______, XXXXXXX, XXXXXXX,  _______, TD(T_LT_HM), KC_UP,   KC_DOWN, TD(T_RT_ED),
-  //|--------+--------+--------+--------+--------+                  +--------+------------+--------+--------+-------------|
-      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F6,   XXXXXXX, XXXXXXX,  KC_F5,   KC_F10,      KC_F11,  KC_F12,  RCTL(KC_F12),
-  //,--------+--------+--------+--------+--------+                  +--------+------------+--------+--------+-------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX,  _______, KC_F8,       XXXXXXX, XXXXXXX, XXXXXXX
+  [_NAV] = LAYOUT_planck_grid(
+    layout.nav[0][0], layout.nav[0][1], layout.nav[0][2], layout.nav[0][3], layout.nav[0][4], XXXXXXX, XXXXXXX, layout.nav[0][5], layout.nav[0][6], layout.nav[0][7], layout.nav[0][8], layout.nav[0][9],
+    layout.nav[1][0], layout.nav[1][1], layout.nav[1][2], layout.nav[1][3], layout.nav[1][4], XXXXXXX, XXXXXXX, layout.nav[1][5], layout.nav[1][6], layout.nav[1][7], layout.nav[1][8], layout.nav[1][9],
+    layout.nav[2][0], layout.nav[2][1], layout.nav[2][2], layout.nav[2][3], layout.nav[2][4], XXXXXXX, XXXXXXX, layout.nav[2][5], layout.nav[2][6], layout.nav[2][7], layout.nav[2][8], layout.nav[2][9],
+    XXXXXXX,           XXXXXXX,         XXXXXXX,          layout.nav[3][0], layout.nav[3][1], XXXXXXX, XXXXXXX, layout.nav[3][2], layout.nav[3][3], XXXXXXX,          XXXXXXX,          XXXXXXX
+  ),
+  [_SYMR] = LAYOUT_planck_grid(
+    layout.symr[0][0], layout.symr[0][1], layout.symr[0][2], layout.symr[0][3], layout.symr[0][4], XXXXXXX, XXXXXXX, layout.symr[0][5], layout.symr[0][6], layout.symr[0][7], layout.symr[0][8], layout.symr[0][9],
+    layout.symr[1][0], layout.symr[1][1], layout.symr[1][2], layout.symr[1][3], layout.symr[1][4], XXXXXXX, XXXXXXX, layout.symr[1][5], layout.symr[1][6], layout.symr[1][7], layout.symr[1][8], layout.symr[1][9],
+    layout.symr[2][0], layout.symr[2][1], layout.symr[2][2], layout.symr[2][3], layout.symr[2][4], XXXXXXX, XXXXXXX, layout.symr[2][5], layout.symr[2][6], layout.symr[2][7], layout.symr[2][8], layout.symr[2][9],
+    XXXXXXX,           XXXXXXX,           XXXXXXX,           layout.symr[3][0], layout.symr[3][1], XXXXXXX, XXXXXXX, layout.symr[3][2], layout.symr[3][3], XXXXXXX,           XXXXXXX,           XXXXXXX
+  ),
+  [_SYML] = LAYOUT_planck_grid(
+    layout.syml[0][0], layout.syml[0][1], layout.syml[0][2], layout.syml[0][3], layout.syml[0][4], XXXXXXX, XXXXXXX, layout.syml[0][5], layout.syml[0][6], layout.syml[0][7], layout.syml[0][8], layout.syml[0][9],
+    layout.syml[1][0], layout.syml[1][1], layout.syml[1][2], layout.syml[1][3], layout.syml[1][4], XXXXXXX, XXXXXXX, layout.syml[1][5], layout.syml[1][6], layout.syml[1][7], layout.syml[1][8], layout.syml[1][9],
+    layout.syml[2][0], layout.syml[2][1], layout.syml[2][2], layout.syml[2][3], layout.syml[2][4], XXXXXXX, XXXXXXX, layout.syml[2][5], layout.syml[2][6], layout.syml[2][7], layout.syml[2][8], layout.syml[2][9],
+    XXXXXXX,           XXXXXXX,           XXXXXXX,           layout.syml[3][0], layout.syml[3][1], XXXXXXX, XXXXXXX, layout.syml[3][2], layout.syml[3][3], XXXXXXX,           XXXXXXX,           XXXXXXX
   )
 };
