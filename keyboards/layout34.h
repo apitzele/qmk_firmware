9ee04ef7f16d719742ec40e43b14f87eb23006ec
Adam Pitzele <Adam Pitzele@outlook.com>
2025-02-15 15:34:10 -0500

    update layout

--- keyboards/layout34.h
+++ keyboards/layout34.h
@@ -18,16 +18,16 @@ extern const struct KeycodeLayout layout;
 const struct KeycodeLayout layout = {
     { // Default
         {
-            TD(T_Q),      TD(T_W),      TD(T_F),      KC_P,           KC_B,     KC_J,            KC_L,           KC_U,         KC_Y,         KC_SCLN
+            TD(T_Q),      TD(T_W),      TD(T_F),      KC_P,              KC_B,            KC_J,            KC_L,           KC_U,         KC_Y,         KC_SCLN
         },
         {
-            LCTL_T(KC_A), LSFT_T(KC_R), LALT_T(KC_S), LT(SYMR, KC_T), KC_G,     KC_M,            LT(SYML, KC_N), LALT_T(KC_E), RSFT_T(KC_I), RCTL_T(KC_O)
+            LCTL_T(KC_A), LALT_T(KC_R), LGUI_T(KC_S), LSFT_T(KC_T),      KC_G,            KC_M,            RSFT_T(KC_N),  RGUI_T(KC_E), LALT_T(KC_I), RCTL_T(KC_O)
         },
         {
-            KC_Z,         TD(T_X),      TD(T_C),      TD(T_D),        TD(T_V),  KC_K,            TD(T_H),        KC_COMM,      KC_DOT,       TD(T_SLASH)
+            KC_Z,         TD(T_X),      TD(T_C),      TD(T_D),           TD(T_V),         KC_K,            TD(T_H),        KC_COMM,      KC_DOT,       TD(T_SLASH)
         },
         {
-                                                      KC_BSPC, LT(NUM, KC_SPC), LT(NAV, KC_ENT), TD(T_DEL)
+                                                      LT(SYMR, KC_BSPC), LT(NUM, KC_SPC), LT(NAV, KC_ENT), LT(SYML, T_DEL)
         }
     },
     { // Num
@@ -35,7 +35,7 @@ const struct KeycodeLayout layout = {
             QK_BOOT,    _______,    _______, _______, _______, KC_PAST, KC_P7,  KC_P8,   KC_P9,   KC_PSLS
         },
         {
-            KC_LCTL,    KC_LSFT,    KC_LALT, KC_LGUI, _______, KC_PPLS, KC_P4,  KC_P5,   KC_P6,   KC_PMNS
+            KC_LCTL,    KC_LALT,    KC_LGUI, KC_LSFT, _______, KC_PPLS, KC_P4,  KC_P5,   KC_P6,   KC_PMNS
         },
         {
             LCTL(KC_Z), LCTL(KC_Y), _______, _______, _______, KC_PDOT, KC_P1,  KC_P2,   KC_P3,   KC_PEQL
@@ -46,16 +46,16 @@ const struct KeycodeLayout layout = {
     },
     { // Nav
         {
-            _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, KC_END
+            _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END
         },
         {
-            KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, _______, KC_HOME, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT
+            KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______, KC_F8,   KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT
         },
         {
             KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F6,   KC_F5,   KC_F10,  KC_F11,  KC_F12,  RCTL(KC_F12)
         },
         {
-                                       _______, _______, _______, KC_F8
+                                       _______, _______, _______, _______
         }
     },
     { // Symbols Right
@@ -63,13 +63,13 @@ const struct KeycodeLayout layout = {
             _______, _______, _______, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN
         },
         {
-            KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, _______, _______, KC_LCBR, KC_RCBR, KC_QUOT, KC_DQT
+            KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______, KC_PIPE, KC_LCBR, KC_RCBR, KC_QUOT, KC_DQT
         },
         {
-            _______, _______, _______, _______, _______,  _______, KC_LBRC, KC_RBRC, KC_GRV,  KC_BSLS
+            _______, _______, _______, _______, _______, KC_TILD, KC_LBRC, KC_RBRC, KC_GRV,  KC_BSLS
         },
         {
-                                       _______, _______, KC_ENT, KC_DEL
+                                       _______, KC_SPC,  KC_ENT, KC_DEL
         }
     },
     { // Symbols Left
@@ -77,7 +77,7 @@ const struct KeycodeLayout layout = {
             KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, _______, _______, _______
         },
         {
-            KC_EQL,  KC_PLUS, KC_MINS, KC_UNDS, _______, KC_RGUI, KC_RALT, KC_RSFT, KC_RCTL, _______
+            KC_EQL,  KC_PLUS, KC_MINS, KC_UNDS, _______, _______, KC_RSFT, KC_RGUI, KC_LALT, KC_RCTL
         },
         {
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
