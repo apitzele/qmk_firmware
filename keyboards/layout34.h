1a91a147382877f79b0005a3e64952a6b89ad39c
Adam Pitzele <Adam Pitzele@outlook.com>
2025-04-03 10:33:49 -0400

    fix symbols

--- keyboards/layout34.h
+++ keyboards/layout34.h
@@ -81,7 +81,7 @@ const struct KeycodeLayout layout = {
     },
     { // Symbols
         {
-            _______, _______, _______, _______, _______, KC_CIRC, KC_AMPR,    KC_UNDS,    KC_LPRN,    KC_RPRN
+            KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_UNDS,    KC_LPRN,    KC_RPRN
         },
         {
             KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, _______, KC_PIPE, KC_LCBR,    KC_RCBR,    TD(T_QUOT), KC_DQT
