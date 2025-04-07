5a30ef202cc4bd641fba46d3c7b358705bea3ad2
Adam Pitzele <Adam Pitzele@outlook.com>
2025-04-07 10:00:05 -0400

    update layout

--- keyboards/layout34.h
+++ keyboards/layout34.h
@@ -53,16 +53,16 @@ const struct KeycodeLayout layout = {
     },
     { // Num
         {
-            QK_BOOT,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  KC_PAST, KC_P7,  KC_P8,   KC_P9,   KC_PSLS
+            QK_BOOT,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_P7,  KC_P8,   KC_P9,   XXXXXXX
         },
         {
-            XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  KC_PPLS, KC_P4,  KC_P5,   KC_P6,   KC_PMNS
+            XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_P4,  KC_P5,   KC_P6,   XXXXXXX
         },
         {
-            LCTL(KC_Z), LCTL(KC_Y), XXXXXXX, XXXXXXX, XXXXXXX,  KC_EQL,  KC_P1,  KC_P2,   KC_P3,   KC_PDOT
+            LCTL(KC_Z), LCTL(KC_Y), XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  KC_P1,  KC_P2,   KC_P3,  XXXXXXX
         },
         {
-                                             XXXXXXX,  XXXXXXX, KC_ENT, KC_P0  
+                                             XXXXXXX,  XXXXXXX, KC_P0, KC_PDOT  
         }
     },
     { // Nav / FN
