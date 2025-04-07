5861e4527f0fa4a432a7f7efde48a8c26b141b2a
apitzele <adam.pitzele@outlook.com>
2025-04-06 22:50:29 -0400

    update layout

--- keyboards/layout34.h
+++ keyboards/layout34.h
@@ -53,44 +53,44 @@ const struct KeycodeLayout layout = {
     },
     { // Num
         {
-            QK_BOOT,    _______,    _______, _______, _______,  KC_PAST, KC_P7,  KC_P8,   KC_P9,   KC_PSLS
+            QK_BOOT,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  KC_PAST, KC_P7,  KC_P8,   KC_P9,   KC_PSLS
         },
         {
-            KC_LCTL,    KC_LSFT,    KC_LGUI, KC_LALT, _______,  KC_PPLS, KC_P4,  KC_P5,   KC_P6,   KC_PMNS
+            XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,  KC_PPLS, KC_P4,  KC_P5,   KC_P6,   KC_PMNS
         },
         {
-            LCTL(KC_Z), LCTL(KC_Y), _______, _______, _______,  KC_EQL, KC_P1,  KC_P2,   KC_P3,   KC_PDOT
+            LCTL(KC_Z), LCTL(KC_Y), XXXXXXX, XXXXXXX, XXXXXXX,  KC_EQL,  KC_P1,  KC_P2,   KC_P3,   KC_PDOT
         },
         {
-                                             _______,  _______, KC_ENT, KC_P0  
+                                             XXXXXXX,  XXXXXXX, KC_ENT, KC_P0  
         }
     },
-    { // Nav
+    { // Nav / FN
         {
-            KC_NUM,  KC_SCRL, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END
+            KC_NUM,  KC_SCRL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN, KC_END
         },
         {
-            KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, _______, KC_F8,   KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT
+            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F8,   KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT
         },
         {
             KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F6,   KC_F5,   KC_F10,  KC_F11,  KC_F12,  RCTL(KC_F12)
         },
         {
-                                       _______, _______, _______, _______
+                                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
         }
     },
     { // Symbols
         {
-            KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_UNDS,    KC_LPRN,    KC_RPRN
+            KC_EXLM, KC_AT,   KC_HASH,  KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_UNDS,    KC_LPRN,    KC_RPRN
         },
         {
-            KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, _______, KC_PIPE, KC_LCBR,    KC_RCBR,    TD(T_QUOT), KC_DQT
+            KC_PPLS, KC_EQL,  KC_MINUS, KC_UNDS, XXXXXXX, KC_PIPE, KC_LCBR,    KC_RCBR,    TD(T_QUOT), KC_DQT
         },
         {
-            _______, _______, _______, _______, _______, KC_TILD, TD(T_LBRC), TD(T_RBRC), TD(T_GRV),  TD(T_BSLS)
+            XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, KC_TILD, TD(T_LBRC), TD(T_RBRC), TD(T_GRV),  TD(T_BSLS)
         },
         {
-                                       _______, KC_SPC,  KC_ENT, KC_DEL
+                                        XXXXXXX, KC_SPC,  KC_ENT, KC_DEL
         }
     }
 };
